1a8fca4fb4e2425b5bcec841baf460a5c79d9e82
饭桶狼 ra～ <96805917+fantonglang@users.noreply.github.com>
2022-02-15 03:53:32 +0800

    .

--- src/ubuntu/install/tigervnc.sh
+++ src/ubuntu/install/tigervnc.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "Install TigerVNC server"
 # wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
-wget https://jaist.dl.sourceforge.net/project/tigervnc/stable/1.12.0/ubuntu-20.04LTS/amd64/tigervncserver_1.12.0-1ubuntu1_amd64.deb
+wget https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.8.0-Linux-x64.deb
 apt-get install -y gdebi-core
-gdebi --n tigervncserver_1.12.0-1ubuntu1_amd64.deb
+gdebi --n VNC-Server-6.8.0-Linux-x64.deb
